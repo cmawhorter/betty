21766064372271df14e289e7fc64ad3fe0a6e794
Cory Mawhorter <cory.mawhorter@gmail.com>
2017-01-23 12:05:34 -0800

    closes #4

--- cli/commands/deploy.js
+++ cli/commands/deploy.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const async      = require('async');
+const waterfall  = require('waterfall');
 const deepAssign = require('deep-assign');
 const buildCmd   = require('./build.js');
 const updateCmd  = require('./update.js');
@@ -9,7 +9,7 @@ exports.command = 'deploy';
 exports.desc    = 'Runs build and update in succession';
 exports.builder = deepAssign({}, buildCmd.builder, updateCmd.builder);
 exports.handler = function(argv) {
-  async.waterfall([
+  waterfall([
     (next) => buildCmd.handler(argv).then(next, next),
     (next) => updateCmd.handler(argv).then(next, next),
   ], (err) => process.exit(err ? 1 : 0));
