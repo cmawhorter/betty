97f5080d1d71d6f18a4725bda898a569c888be99
Cory Mawhorter <cory.mawhorter@gmail.com>
2017-01-25 13:06:30 -0800

    closes #18

--- cli/commands/logs.js
+++ cli/commands/logs.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const path = require('path');
+const spawn = require('child_process').spawn;
+const createHandler = require('../lib/command-handler.js');
+
 exports.command = 'logs';
 exports.desc    = 'Streams the cloudwatch log for the function';
 exports.builder = {
@@ -7,10 +11,28 @@ exports.builder = {
     describe:       'AWS region to target. Defaults to $AWS_REGION',
     default:        process.env.AWS_REGION,
   },
+  profile: {
+    describe:       'AWS credentials profile to target. Defaults to $AWS_PROFILE',
+    default:        process.env.AWS_PROFILE,
+  },
+  name: {
+    alias:          'n',
+    describe:       'The CloudWatch log name.  Defaults to the project.name',
+  },
 };
-exports.handler = function(argv) {
+exports.handler = createHandler((argv, done) => {
+  const cmd = path.join(process.cwd(), './node_modules/.bin', 'pbcw');
+  const cmdArgs = [
+    `-p${argv.profile}`,
+    `-f`,
+    `/aws/lambda/${argv.n || argv.config.name}`
+  ];
   process.env.AWS_REGION = argv.region;
-  process.argv.push(`-p ${process.env.AWS_PROFILE}`);
-  process.argv.push(`-f /aws/lambda/${argv.config.name}`);
-  require('pbcw');
-};
+  console.log('pbcw cmd', cmd);
+  console.log('pbcw args', cmdArgs);
+  const pbcw = spawn(cmd, cmdArgs, {
+    stdio:          'inherit',
+    cwd:            process.cwd(),
+  });
+  done(null);
+});
