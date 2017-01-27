d1359123689426fcbd4f318ad19739a7054a28c1
Cory Mawhorter <cory.mawhorter@gmail.com>
2017-01-26 21:34:45 -0800

    rc wip

--- cli/index.js
+++ cli/index.js
@@ -7,11 +7,20 @@ require('../common/env.js');
 const resource      = require('../common/resource.js');
 const getAccountId  = require('../common/account-id.js');
 
-// before processing command, get the account id associated with the current user
-// to be best-practice used in naming things like s3 buckets
-getAccountId((err, accountId) => {
-  if (err) throw err;
-  global.BETTY.aws.accountId = accountId;
+const boot = () => {
   global.config = resource.load(process.cwd(), 'resource');
   require('./cli.js');
-});
+};
+
+if (global.BETTY.aws.accountId) {
+  boot();
+}
+else {
+  // before processing command, get the account id associated with the current user
+  // to be best-practice used in naming things like s3 buckets
+  getAccountId((err, accountId) => {
+    if (err) throw err;
+    global.BETTY.aws.accountId = accountId;
+    boot();
+  });
+}
--- common/env.js
+++ common/env.js
@@ -1,12 +1,13 @@
 'use strict';
 
-global.BETTY = {
-  env:            process.env.NODE_ENV,
-  cwd:            process.cwd(),
+global.BETTY = require('rc')('betty', {
   aws: {
-    accountId:    'unknown',
+    accountId:    null,
     region:       process.env.AWS_REGION,
   },
-};
+});
+
+global.BETTY.env = process.env.NODE_ENV;
+global.BETTY.cwd = process.cwd();
 
 require('./log.js');
