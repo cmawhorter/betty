7c86284ecfa86effd03e66fc4ad9132e1ac74173
Cory Mawhorter <cory.mawhorter@gmail.com>
2017-01-26 18:56:59 -0800

    use stdout

--- cli/commands/info.js
+++ cli/commands/info.js
@@ -6,5 +6,5 @@ exports.command = 'info';
 exports.desc    = 'Outputs info about the current project context';
 exports.builder = {};
 exports.handler = createHandler(function(argv) {
-  console.log('Config:', JSON.stringify(global.config, null, 2));
+  process.stdout.write(JSON.stringify(global.config, null, 2));
 });
