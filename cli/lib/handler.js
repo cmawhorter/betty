631c36f2c60482ae9aefa751daee317d304b8c7e
Cory Mawhorter <cory.mawhorter@gmail.com>
2017-02-06 13:21:25 -0800

    unhandled command error log

--- cli/lib/handler.js
+++ cli/lib/handler.js
@@ -2,7 +2,11 @@
 
 module.exports = (handler) => {
   return (argv, callback) => {
-    callback = callback || function(){};
+    callback = callback || function(err) {
+      if (err) {
+        global.log.error({ err }, 'command completed with error');
+      }
+    };
     return handler(argv, callback);
     if (handler.length === 1) { // handler doesn't have a callback which means it's sync
       callback(null);
